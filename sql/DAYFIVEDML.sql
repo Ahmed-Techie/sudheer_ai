554a3febfa3538d5981556a1d14f81d1bec0a474
Ahmed-Techie <118879467+Ahmed-Techie@users.noreply.github.com>
2026-04-04 23:51:37 +0530

    Add files via upload

---
+++ sql/DAYFIVEDML.sql
@@ -0,0 +1,20 @@
+                                                   -- DML
+                                                   -- DATA MANIPULATION LANGUAGE
+                                                   -- INSERT -- UPDATE -- DELETE
+                                                   
+                                                   -- TCL - TRANSACTION CONTROL LANGUAGE
+                                                   -- ROLL BACK, COMMIT, SAVEPOINT
+USE HR;
+
+SELECT *
+FROM EMPLOYEES;
+
+USE MYDB;
+SELECT * 
+FROM STUDENTS;
+
+
+INSERT INTO STUDENTS(NAME, DOB, PHONE_NUMBER,COURSE) VALUES("AAKASH","2000-10-10","8765876500","DATA ANALYTICS");
+
+INSERT INTO STUDENTS(NAME, DOB, PHONE_NUMBER,COURSE) VALUES("ABC", "1999-02-02", "9090902345","DATA ANALYTICS");
+
